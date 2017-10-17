271a45db6fa7929e9d1a2cf7c4a135a49122ea8b
Roxanne <argonkryptonneon@gmail.com>
2017-10-17 10:04:08 -0700

    added logic to only allow works to be edited if they were created by the user

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -2,7 +2,7 @@ class WorksController < ApplicationController
   # We should always be able to tell what category
   # of work we're dealing with
   before_action :category_from_work, except: [:root, :index, :new, :create]
-  before_action :creator, only:[:edit, :update, :destroy]
+  before_action :crefator, only:[:edit, :update, :destroy]
   skip_before_action :require_login, only: [:root]
 
   def root
